5adbbdff0c5ed5623ac731667fd9823ae3b1b55e
Barun Sharma <sharma.barun2@gmail.com>
2019-04-24 19:28:59 -0400

    Methods to find duplicate and unique values
    Methods to find duplicate and unique values from an array

--- find-duplicates.rb
+++ find-duplicates.rb
@@ -22,3 +22,31 @@
 #
 #     output_unique_members_array = [ 5, 9, 20, 3, 4, 11 ]
 #
+
+require 'rspec/autorun'
+
+# Return with element with duplicate
+def find_duplicate(arr)
+	arr.select{|e| arr.count(e) > 1}.uniq
+end
+
+# Return array with unique values
+def find_uniq(arr)
+	arr.uniq
+end
+
+
+RSpec.describe do
+
+	let(:array_1) {["computer", "pencil", "paper", "computer", "pen", "pencil"]}
+	let(:array_2) {[ 5, 9, 20, 20, 3, 4, 5, 11 ]}
+
+	it "should return array with duplicates value" do
+		expect(["computer", "pencil"]).to eq(find_duplicate(array_1))
+	end
+
+	it "should return array with unique values" do
+		expect([ 5, 9, 20, 3, 4, 11 ]).to eq(find_uniq(array_2))
+	end
+
+end
