4f4455d3f1c14915a6273ecdd1bbbe344bf0f277
Joey Ferris <joey@joeyferris.io>
2019-04-23 20:07:51 -0500

    Initial commit

---
+++ find-duplicates.rb
@@ -0,0 +1,24 @@
+#
+# 1. Write a function to find all duplicate members of the below array
+#    and return a new array with only duplicates.
+#
+#     Input:
+#
+#     input_duplicates_array = ['computer', 'pencil', 'paper, 'computer', 'pen', 'pencil']
+#
+#     Output:
+#
+#     output_duplicates_array = ['computer', 'pencil']
+#
+#
+# 2. Write a function to remove duplicate members of the below array and
+#    return a new array with non-duplicates.
+#
+#     Input:
+#
+#     input_unique_members_array = [ 5, 9, 20, 20, 3, 4, 5, 11 ]
+#
+#     Output:
+#
+#     output_unique_members_array = [ 5, 9, 20, 3, 4, 11 ]
+#
